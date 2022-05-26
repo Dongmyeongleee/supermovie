e794dd8be7ad63dafbc4d72491baaafa1c14b5f2
이동명 <aklsjd7380@naver.com>
2022-05-26 17:44:23 +0900

    admin

--- django-back/movies/admin.py
+++ django-back/movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Movie, Genre, UserRank, Review
 # Register your models here.
+
+
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(UserRank)
+admin.site.register(Review)
